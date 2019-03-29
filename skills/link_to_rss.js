e8a719a06d95482c2795b1060ecea4bbdae31aa3
Eric Allen <eric@ericrallen.dev>
2019-03-28 22:38:43 -0400

    add bot join listener

--- skills/link_to_rss.js
+++ skills/link_to_rss.js
@@ -1,5 +1,9 @@
 module.exports = function(controller) {
-  controller.hears('(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?', 'message, message.channels', function(bot, message) {
+  controller.on('bot_channel_join', function(bot, message) {
+    bot.reply(message, 'Hey there! I\'m here to scrape links and generate an RSS feed for you.');
+  });
+
+  controller.hears(['(https?://)?(\w+\.)?(\w)+\.\w+\/?'], 'message, message.channels', function(bot, message) {
     console.log('heard a URL:', message);
     bot.reply(message, 'There was a URL in there.');
   });
