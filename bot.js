01259d73e68a6f83c20791ec99b921bb32843867
Eric Allen <eric@ericrallen.dev>
2019-03-28 21:40:56 -0400

    removed node-env-file

--- bot.js
+++ bot.js
@@ -47,9 +47,6 @@ This bot demonstrates many of the core features of Botkit:
     -> http://howdy.ai/botkit
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-var env = require('node-env-file');
-env(__dirname + '/.env');
-
 
 if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
   usage_tip();
