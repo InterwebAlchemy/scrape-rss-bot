6963c55b21d387465e5df22667b019c478ec6659
Eric Allen <eric@ericrallen.dev>
2019-03-31 18:48:59 -0400

    fixed ENV name for MONGODB_URI

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ var bot_options = {
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
 if (process.env.MONGODB_URI) {
-    var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGO_URI});
+    var mongoStorage = require('botkit-storage-mongo')({mongoUri: process.env.MONGODB_URI});
     bot_options.storage = mongoStorage;
 } else {
     bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
