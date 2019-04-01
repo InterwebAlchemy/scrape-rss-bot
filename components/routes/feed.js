5b4e7b959930fd39131a0ccde55690ab9064f8fd
Eric Allen <eric@ericrallen.dev>
2019-03-31 23:06:41 -0400

    updating rss handling for dates; trying to filter and sort links in feed

--- components/routes/feed.js
+++ components/routes/feed.js
@@ -19,46 +19,52 @@ module.exports = function(webserver, controller) {
       query.channelName = channelName;
     }
 
-    controller.storage.links.find(query, function(err, links) {
-      if (err) {
-        debug('Error: could not retrieve links:', err);
+    controller.storage.links
+      .find(query, function(err, links) {
+        if (err) {
+          debug('Error: could not retrieve links:', err);
 
-        res.send("There was an error retrieving your RSS Feed.");
-      }
+          res.send("There was an error retrieving your RSS Feed.");
+        }
 
-      const { teamName } = links[0];
+        const { teamName, teamUrl } = links[0];
 
-      let feedTitle = `${teamName} Slack RSS Feed`;
+        let feedTitle = `${teamName} Slack RSS Feed`;
 
-      if (channelName) {
-        feedTitle = `${feedTitle} for #${channelName}`;
-      }
+        if (channelName) {
+          feedTitle = `${feedTitle} for #${channelName}`;
+        }
 
-      let feedDescription = `Links posted in the`;
+        let feedDescription = `Links posted in the`;
 
-      if (channelName) {
-        feedDescription = `${feedDescription} #${channelName} channel.`
-      } else {
-        feedDescription = `${feedDescription} ${teamName} Slack.`
-      }
+        if (channelName) {
+          feedDescription = `${feedDescription} #${channelName} channel.`
+        } else {
+          feedDescription = `${feedDescription} ${teamName} Slack.`
+        }
 
-      let feedUrl = getFeed(teamId, channelName);
+        let feedUrl = getFeed(teamId, channelName);
 
-      const categories = [...new Set(links.map(({ categories }) => [...categories]).flat())];
+        const categories = [...new Set(links.map(({ categories }) => [...categories]).flat())];
 
-      const feed = new RSS({
-        title: feedTitle,
-        description: feedDescription,
-        feed_url: feedUrl,
-        site_url: 'https://skookum.slack.com/',
-        categories,
-      });
+        const feed = new RSS({
+          title: feedTitle,
+          description: feedDescription,
+          feed_url: feedUrl,
+          site_url: teamUrl,
+          categories,
+        });
 
-      links.forEach(({ item }) => {
-        feed.item(item);
-      });
+        links.forEach(({ item }) => {
+          feed.item(item);
+        });
 
-      res.send(feed.xml());
-    });
+        res.send(feed.xml());
+      })
+      .sort({
+        shareDate: -1
+      })
+      .limit(10)
+    ;
   });
 }
--- skills/link_to_rss.js
+++ skills/link_to_rss.js
@@ -42,15 +42,19 @@ module.exports = function(controller) {
 
         bot.replyInteractive(message, `Adding to RSS Feed...`);
 
-        console.log(bot);
+        const date = new Date();
 
-        /*scrape(url)
+        scrape(url)
           .then((meta) => {
-            const item = Object.assign({}, meta, { categories: [`#${channelName}`] });
+            const item = Object.assign({}, meta, { categories: [`#${channelName}`], date: date.toISOString() });
+
+
 
             const link = {
               teamId: bot.team_info.id,
               teamName: bot.team_info.name,
+              teamUrl: bot.team_info.url,
+              shareDate: date.getTime(),
               channelName,
               item,
             };
@@ -63,7 +67,7 @@ module.exports = function(controller) {
               bot.replyInteractive(message, `:+1: I've added this link to the RSS Feed.`);
             });
           })
-        ;*/
+        ;
       });
     }
   });
