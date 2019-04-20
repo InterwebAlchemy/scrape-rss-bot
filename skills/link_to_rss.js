caad559e0a141bc74dd60556ef3e5c3492d9b2f7
Eric Allen <eric@ericrallen.dev>
2019-04-20 14:56:32 -0400

    feat(bot): add handling of multiple links in a message
    Closes #8

--- skills/link_to_rss.js
+++ skills/link_to_rss.js
@@ -4,6 +4,57 @@ const scrape = require('../utils/scrape');
 const getFeed = require('../utils/rss-link');
 const getChannel = require('../utils/get-channel-name');
 
+const GLOBAL_URL_REGEX = /<(https?:\/\/[-A-Z0-9._~:\/?#[\]@!$&'()*+,;=]+)>/igm;
+const URL_REGEX = /<(https?:\/\/[-A-Z0-9._~:\/?#[\]@!$&'()*+,;=]+)>/i;
+
+const URLS_TO_ADD = [];
+
+const processQueue = () => {
+  if (URLS_TO_ADD.length) {
+    const { url, message, channelName } = URLS_TO_ADD.shift();
+
+    addUrlToFeed(url, message, channelName);
+  }
+}
+
+const addUrlToFeed = (url, message, channelName) => {
+  const content = {
+    attachments:[
+      {
+        title: `Would you like to add ${url} to the #${channelName} RSS Feed?`,
+        callback_id: 'ADD_TO_RSS',
+        attachment_type: 'default',
+        actions: [
+          {
+            "name": "add_to_rss",
+            "text": `+ Add to RSS Feed`,
+            "value": `${message.ts}:|:${url}`,
+            "type": "button",
+          },
+          {
+            "name": "not_to_rss",
+            "text": `No Thanks`,
+            "value": 'NO',
+            "type": "button",
+          },
+        ]
+      }
+    ]
+  };
+
+  // check to see if we are in a thread
+  if (message.thread_ts) {
+    // if we are, let's grab the thread's timestamp, so we can reply inline
+    const { thread_ts } = message;
+
+    // reply ephemerally inline
+    bot.whisper(message, Object.assign({}, { thread_ts }, content));
+  // otherwise just reply as normal
+  } else {
+    bot.whisper(message, content);
+  }
+};
+
 module.exports = function(controller) {
   controller.on('bot_channel_join', function(bot, message) {
     getChannel(bot, message, (channelName, channelId) => {
@@ -20,44 +71,19 @@ module.exports = function(controller) {
   });
 
   controller.hears(['<(https?:\\/\\/[-A-Za-z0-9\\._~:\\/?#[\\]@!$&\'()\\*\\+,;=]+)>'], 'ambient', function(bot, message) {
-    const url = message.match[1];
-
     getChannel(bot, message, (channelName, channelId) => {
-      const content = {
-        attachments:[
-          {
-            title: `Would you like to add ${url} to the #${channelName} RSS Feed?`,
-            callback_id: 'ADD_TO_RSS',
-            attachment_type: 'default',
-            actions: [
-              {
-                "name": "add_to_rss",
-                "text": `+ Add to RSS Feed`,
-                "value": `${message.ts}:|:${url}`,
-                "type": "button",
-              },
-              {
-                "name": "not_to_rss",
-                "text": `No Thanks`,
-                "value": 'NO',
-                "type": "button",
-              },
-            ]
-          }
-        ]
-      };
+      // TODO: replace with native String.prototype.matchAll() when possible
+      const checkForMultipleUrls = message.text.match(GLOBAL_URL_REGEX);
 
-      // check to see if we are in a thread
-      if (message.thread_ts) {
-        // if we are, let's grab the thread's timestamp, so we can reply inline
-        const { thread_ts } = message;
+      checkForMultipleUrls.forEach((foundUrl) => {
+        const url = URL_REGEX.exec(foundUrl)[1];
 
-        // reply ephemerally inline
-        bot.whisper(message, Object.assign({}, { thread_ts }, content));
-      // otherwise just reply as normal
-      } else {
-        bot.whisper(message, content);
-      }
+        if (url) {
+          URLS_TO_ADD.push({ url, message, channelName });
+        }
+      });
+
+      processQueue();
     });
   });
 
@@ -72,6 +98,8 @@ module.exports = function(controller) {
           'replace_original': true,
           'delete_original': true
         });
+
+        processQueue();
       } else {
         const [ timestamp, url ] = value.split(':|:');
 
@@ -100,9 +128,18 @@ module.exports = function(controller) {
                   debug('Error: could not save link record:', err);
                 }
 
+                let timeoutInterval = 1500;
+                let nextIteration;
+
                 bot.replyInteractive(message, `:+1: I've added this link to the <${getFeed(bot.team_info.id, channelId)}|#${channelName} RSS Feed>.`);
 
                 bot.api.reactions.add({ channel: message.channel, name: 'book', timestamp });
+
+                if (URLS_TO_ADD.length) {
+                  timeoutInterval = 500;
+
+                  nextIteration = processQueue();
+                }
 
                 setTimeout(function() {
                   bot.replyInteractive(message, {
@@ -111,7 +148,11 @@ module.exports = function(controller) {
                     'replace_original': true,
                     'delete_original': true
                   });
-                }, 2500);
+
+                  if (typeof nextIteration === 'function') {
+                    nextIteration();
+                  }
+                }, timeoutInterval);
               });
             })
             .catch((error) => {
