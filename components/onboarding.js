d08ea55a7f0b0ca4a3a4c4aaef5046ad5f538738
Eric Allen <eric@ericrallen.dev>
2019-05-25 03:39:19 -0400

    finishing polish on success and error pages and onboarding

--- components/onboarding.js
+++ components/onboarding.js
@@ -6,7 +6,7 @@ module.exports = function(controller) {
       } else {
         convo.say('Hey there! I\'m here to help you out.');
         convo.say('Just `/invite` me to a channel so I can start building an RSS Feed when links are posted.');
-        convo.say('Message me with `help` if you have any questions.');
+        convo.say('Use the `/rsshelp` if you have any questions.');
       }
     });
   });
