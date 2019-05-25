34c7192609751839bb0ee0eb95fde2ea64ecbd47
Eric Allen <eric@ericrallen.dev>
2019-05-25 09:24:05 -0400

    fix(bot): add as_user to initial ephemeral message so avatar shows correctly

--- skills/link_to_rss.js
+++ skills/link_to_rss.js
@@ -49,10 +49,10 @@ const addUrlToFeed = (url, message, channelName) => {
     const { thread_ts } = message;
 
     // reply ephemerally inline
-    bot.whisper(message, Object.assign({}, { thread_ts }, content));
+    bot.whisper(message, Object.assign({}, { thread_ts, as_user: false }, content));
   // otherwise just reply as normal
   } else {
-    bot.whisper(message, content);
+    bot.whisper(message, Object.assign({}, { as_user: false }, content));
   }
 };
 
