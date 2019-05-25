c74227d65b74302d6d29295d4005a953e18fee5a
Eric Allen <eric@ericrallen.dev>
2019-05-25 01:20:49 -0400

    feat(site): add /help page to satisfy Slack support requirements

--- bot.js
+++ bot.js
@@ -92,6 +92,14 @@ webserver.get('/', function(req, res){
   });
 });
 
+webserver.get('/help', function(req, res){
+  res.render('help', {
+    domain: req.get('host'),
+    protocol: req.protocol,
+    layout: 'layouts/default'
+  });
+});
+
 // Set up a simple storage backend for keeping a record of customers
 // who sign up for the app via the oauth
 require(__dirname + '/components/user_registration.js')(controller);
